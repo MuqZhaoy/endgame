7cb516046d81fe26533a160d3cd415a2927a0d9a
MqZy <MuqZhaoy@proton.me>
2025-04-16 17:51:24 +0800

    换成测试集

--- src/main.py
+++ src/main.py
@@ -16,21 +16,21 @@ model_name = "meta-llama/Llama-2-7b-hf"
 # model_name = "facebook/opt-30b"
 # model_name = "facebook/opt-66b"
 
-# dataset_name = "Rowan/hellaswag"
+dataset_name = "Rowan/hellaswag"
 # dataset_name = "math_qa"
 # dataset_name = "piqa"
-dataset_name = "truthful_qa"
+# dataset_name = "truthful_qa"
 
 dtype = torch.float16
-question_count = 13
+question_count = 17
 
 
 if __name__ == "__main__":
-    exp.GridSearch(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
+    # exp.GridSearch(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
     # exp.KeyValueDifference(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
     # exp.KVcacheDistribution(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
     # exp.AttentionInsight(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
     # exp.AttentionAwareInsight(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
     # exp.AdaptiveMethodInsight(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
     # exp.GroupingInsight(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
-    # exp.Test(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
+    exp.Test(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
