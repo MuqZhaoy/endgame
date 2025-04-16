3380eb72228b8abc90bb0f1b0e3b07b2c7a9b5b5
MqZy <MuqZhaoy@proton.me>
2025-04-16 17:10:40 +0800

    调小阈值，进行测试

--- src/main.py
+++ src/main.py
@@ -22,15 +22,15 @@ dataset_name = "Rowan/hellaswag"
 # dataset_name = "truthful_qa"
 
 dtype = torch.float16
-question_count = 1000
+question_count = 20
 
 
 if __name__ == "__main__":
-    exp.GridSearch(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
+    # exp.GridSearch(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
     # exp.KeyValueDifference(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
     # exp.KVcacheDistribution(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
     # exp.AttentionInsight(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
     # exp.AttentionAwareInsight(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
     # exp.AdaptiveMethodInsight(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
     # exp.GroupingInsight(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
-    # exp.Test(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
+    exp.Test(model_name, dataset_name, dtype, question_count, parallel=True, verbose=True).run()
